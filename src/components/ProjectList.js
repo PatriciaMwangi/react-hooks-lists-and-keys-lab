934c52e8b6e52f0529bf73f326a3faf29203457b
patricia <patricia.mwangi@student.moringaschool.com>
2024-04-27 13:52:40 +0300

    lists and objects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,29 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  let projectElements=[]
+  for (let project of projects){
+    
+    projectElements.push(
+      <ProjectItem 
+      key={project.id}
+      name={project.name}
+      technologies={project.technologies}
+      about={project.about}
+      />
+    )
+  
+  }
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {projectElements}
+      </div>
+    
     </div>
   );
 }
 
 export default ProjectList;
+
